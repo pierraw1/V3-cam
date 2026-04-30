82910d4780870a4bb9a6f5beb3703c212b46ea44
pierraw1 <peterbacq@gmail.com>
2026-04-30 11:58:25 +0200

    test

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/peterbacq/Desktop/V3-cam/tina/__generated__/.cache/1777542769407', url: 'https://content.tinajs.io/2.2/content/afb77850-f99f-4a0a-8ca6-ba635ca2c706/github/main', token: '6abfeeca541df44bbb0926b7c12a635d355afd0e', queries,  });
+export const client = createClient({ cacheDir: '/Users/peterbacq/Desktop/V3-cam/tina/__generated__/.cache/1777543042501', url: 'https://content.tinajs.io/2.2/content/afb77850-f99f-4a0a-8ca6-ba635ca2c706/github/main', token: '6abfeeca541df44bbb0926b7c12a635d355afd0e', queries,  });
 export default client;
   
